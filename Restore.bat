d9a9dde1bd114d5946ddcc3ddffaf91e9b4f96f5
Jack Gassett <jack.gassett@gmail.com>
2012-06-29 03:09:53 -0600

    First cut at a universal installer.

---
+++ Restore.bat
@@ -0,0 +1,9 @@
+REM This bat file puts all repositories back to master
+
+cd ..\Papilio-Loader
+git checkout master
+
+cd ..\Arcade-Blaster
+git checkout master
+
+pause
---
+++ Setup.bat
@@ -0,0 +1,9 @@
+REM This bat file puts all repositories to the correct version before building a new release
+
+cd ..\Papilio-Loader
+git checkout Papilio_Programmer_V2.1
+
+cd ..\Arcade-Blaster
+git checkout Arcade_Blaster-V1.0
+
+pause
